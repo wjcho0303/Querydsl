a5ff86a0300dd7631c137b3c69ab2635fa680c1a
wjcho <wj930303@gmail.com>
2024-07-16 00:05:35 +0900

    Feat: JPQL과 querydsl의 차이 살펴보기

---
+++ src/test/java/study/querydsl/entity/QuerydslBasicTest.java
@@ -0,0 +1,71 @@
+package study.querydsl.entity;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+public class QuerydslBasicTest {
+
+    @Autowired
+    EntityManager em;
+
+    JPAQueryFactory jpaQueryFactory;
+
+    @BeforeEach
+    public void before() {
+        jpaQueryFactory = new JPAQueryFactory(em);
+        // 트랜잭션에 바인딩 되어 분배되도록 설계되었기 때문에 이렇게 해도 멀티 쓰레드 환경에서 문제가 발생하지 않는다.
+
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+    }
+
+    @Test
+    @DisplayName("JPQL을 통한 조회")
+    public void startJPQL() {
+        // member1 을 찾는 JPQL
+        String query = "select m from Member m where m.username = :username";
+        Member findMember = em.createQuery(query, Member.class)
+                .setParameter("username", "member1")
+                .getSingleResult();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    @DisplayName("Querydsl을 통한 조회")
+    public void startQuerydsl() {
+        QMember m = new QMember("m");
+
+        Member findMember = jpaQueryFactory
+                .select(m)
+                .from(m)
+                .where(m.username.eq("member1"))    // 파라미터 바인딩 자동 처리됨
+                .fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+
+}
