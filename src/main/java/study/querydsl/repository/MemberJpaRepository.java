970ce7f12231ddf0e2226a7cd804e5b8d95a59cc
wjcho <wj930303@gmail.com>
2024-07-18 23:08:51 +0900

    Feat: 순수 JPA와 QueryDSL

---
+++ src/main/java/study/querydsl/repository/MemberJpaRepository.java
@@ -0,0 +1,16 @@
+package study.querydsl.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class MemberJpaRepository {
+
+    private final EntityManager em;
+    private final JPAQueryFactory jpaQueryFactory;
+
+
+}
