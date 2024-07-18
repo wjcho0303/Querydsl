6cd637409b36d85ea8fc9634dca687f15a7ec3f7
wjcho <wj930303@gmail.com>
2024-07-18 23:11:55 +0900

    Feat: 저장 및 조회 메서드 생성

--- src/main/java/study/querydsl/repository/MemberJpaRepository.java
+++ src/main/java/study/querydsl/repository/MemberJpaRepository.java
@@ -4,6 +4,10 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import study.querydsl.entity.Member;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
@@ -12,5 +16,23 @@ public class MemberJpaRepository {
     private final EntityManager em;
     private final JPAQueryFactory jpaQueryFactory;
 
+    public void save(Member member) {
+        em.persist(member);
+    }
 
+    public Optional<Member> findById(Long id) {
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findByUsername(String username) {
+        return em.createQuery("select m from Member m where m.username = :username", Member.class)
+                .setParameter("username", username)
+                .getResultList();
+    }
 }
