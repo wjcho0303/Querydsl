a87b1d2db955e4db5a69838c3201beb76021061c
wjcho <wj930303@gmail.com>
2024-07-19 23:40:33 +0900

    Feat: querydsl 조회 API 예제

--- src/main/java/study/querydsl/controller/InitMember.java
+++ src/main/java/study/querydsl/controller/InitMember.java
@@ -35,7 +35,7 @@ public class InitMember {
             em.persist(teamB);
 
             for (int i = 1; i <= 100; i++) {
-                Team selectedTeam = i % 2 == 0 ? teamA : teamB;
+                Team selectedTeam = i % 2 != 0 ? teamA : teamB;
                 em.persist(new Member("member"+i, i, selectedTeam));
             }
         }
