62a0c4c6b192ae1faf15f62dedefbbac275bb1c0
wjcho <wj930303@gmail.com>
2024-07-20 00:30:23 +0900

    Feat: SpringDataJPA 레포지토리 인터페이스 생성

---
+++ src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -0,0 +1,58 @@
+package study.querydsl.repository;
+
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Member;
+import study.querydsl.entity.Team;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberRepositoryTest {
+
+    @Autowired
+    EntityManager em;
+
+    @Autowired
+    MemberJpaRepository repository;
+
+    @BeforeEach
+    public void before() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+    }
+
+    @Test
+    public void basicTest() {
+        Member member5 = new Member("member5", 10);
+        repository.save(member5);
+
+        Member findMember = repository.findById(member5.getId()).get();
+        assertThat(findMember).isEqualTo(member5);
+
+        List<Member> result1 = repository.findAll();
+        assertThat(result1).contains(member5);
+
+        List<Member> result2 = repository.findByUsername("member5");
+        assertThat(result2).containsExactly(member5);
+    }
+}
