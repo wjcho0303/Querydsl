e66b2aa265e8942aa77b7f2c5c8a5ed5dc6b6e2b
wjcho <wj930303@gmail.com>
2024-07-20 01:41:07 +0900

    Refactor: 변경된 Repository로 갈아끼운 후 api 테스트

--- src/main/java/study/querydsl/controller/MemberController.java
+++ src/main/java/study/querydsl/controller/MemberController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
-import study.querydsl.repository.MemberJpaRepository;
+import study.querydsl.repository.MemberRepository;
 
 import java.util.List;
 
@@ -13,10 +13,10 @@ import java.util.List;
 @RequiredArgsConstructor
 public class MemberController {
 
-    private final MemberJpaRepository memberJpaRepository;
+    private final MemberRepository memberRepository;
 
     @GetMapping("/v1/members")
     public List<MemberTeamDto> searchMemberV1(MemberSearchCondition condition) {
-        return memberJpaRepository.searchByWhereParam(condition);
+        return memberRepository.searchByWhereParam(condition);
     }
 }
